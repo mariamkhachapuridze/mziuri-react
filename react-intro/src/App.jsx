fbb0e32722e5a8eef43ef33566b883ca4d9f2070
mariamkhachapuridze <khachapuridzemariami1@gmail.com>
2025-10-13 20:03:55 +0400

    init

---
+++ react-intro/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+
+const projectName = "React Basics"
+
+
+  return (
+    <>
+      <h1>my first react project  -  name : {projectName}</h1>
+    </>
+  )
+}
+
+export default App
