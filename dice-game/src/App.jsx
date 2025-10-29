2379d76930506b58d8879792f68a9019b688681c
mariamkhachapuridze <khachapuridzemariami1@gmail.com>
2025-10-29 23:06:16 +0400

    styling

--- dice-game/src/App.jsx
+++ dice-game/src/App.jsx
@@ -1,10 +1,10 @@
-import {useState} from "react";
+import { useState } from "react";
 import Dice from "./Dice.jsx";
 function App() {
-  const [player1Total,setPlayer1Total] = useState(0);
-  const [player2Total,setPlayer2Total] = useState(0);
-  const [round,setRound]= useState(0);
-  const [winner,setWinner]= useState("");
+  const [player1Total, setPlayer1Total] = useState(0);
+  const [player2Total, setPlayer2Total] = useState(0);
+  const [round, setRound] = useState(0);
+  const [winner, setWinner] = useState("");
 
   const [player1, setPlayer1] = useState(0);
   const [player2, setPlayer2] = useState(0);
@@ -14,21 +14,21 @@ function App() {
     const playerRandomNumber = Math.floor(Math.random() * 6);
     setWinner("");
 
-    if(currentPlayer === 1){
+    if (currentPlayer === 1) {
       setPlayer1(playerRandomNumber);
-      setPlayer1Total(player1Total + playerRandomNumber+1);
+      setPlayer1Total(player1Total + playerRandomNumber + 1);
       setCurrentPlayer(2);
     } else {
-      let newTotal = player2Total + playerRandomNumber+1;
+      let newTotal = player2Total + playerRandomNumber + 1;
 
       setPlayer2(playerRandomNumber);
       setPlayer2Total(newTotal);
       setCurrentPlayer(1);
       setRound(round + 1);
-      if(round === 1){
-        if(player1Total > newTotal){
+      if (round === 1) {
+        if (player1Total > newTotal) {
           setWinner("Player 1 wins!");
-        } else if (newTotal > player1Total){
+        } else if (newTotal > player1Total) {
           setWinner("Player 2 wins!");
         } else {
           setWinner("It's a tie!");
@@ -40,27 +40,45 @@ function App() {
         setPlayer2(0);
       }
     }
+  };
 
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        padding: "20px",
+        margin: 0,
+      }}
+    >
+      <h1>2-Player Dice Game</h1>
+      <div
+        style={{
+          display: "flex",
+          width: "100%",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Dice
+          title={"Player 1"}
+          index={player1}
+          handleClick={rollDice}
+          isDisabled={currentPlayer === 2}
+        />
 
-  }
-  
-  return <div
-  style={{
-    textAlign: 'center',
-    padding: '20px',
-    margin: 0
-  }}>
-    <h1>2-Player Dice Game</h1>
-    <Dice title={"Player 1"} index={player1} handleClick={rollDice} isDisabled={currentPlayer === 2} />
+        <div>VS</div>
 
-  <div>VS</div>
-  
-    <Dice title={"Player 2"} index={player2} handleClick={rollDice} isDisabled={currentPlayer === 1}/>
-      <p>Player 1 total: {player1Total}</p>
-      <p>Player 2 total: {player2Total}</p>
-      <p>Round: {round }</p>
-      <p>{winner}</p>
-  </div>
+        <Dice
+          title={"Player 2"}
+          index={player2}
+          handleClick={rollDice}
+          isDisabled={currentPlayer === 1}
+        />
+      </div>
+
+      <h1>{winner}</h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- dice-game/src/Dice.jsx
+++ dice-game/src/Dice.jsx
@@ -1,11 +1,13 @@
-export default function Dice({title, index, handleClick, isDisabled}) {
+export default function Dice({ title, index, handleClick, isDisabled }) {
+  const diceEmojis = ["⚀", "⚁", "⚂", "⚃", "⚄", "⚅"];
 
-    const diceEmojis = ["⚀", "⚁", "⚂", "⚃", "⚄", "⚅"];
-
-    return <>
-        <div style={{fontSize: "60px"}}>{diceEmojis[index]}</div>
-        <p>{title}</p>
-        <button onClick={handleClick} disabled={isDisabled}>Roll Dice</button>
-        </>
-    
+  return (
+    <div style={{ textAlign: "center", padding: "20px" }}>
+      <div style={{ fontSize: "60px" }}>{diceEmojis[index]}</div>
+      <p>{title}</p>
+      <button onClick={handleClick} disabled={isDisabled}>
+        Roll Dice
+      </button>
+    </div>
+  );
 }
