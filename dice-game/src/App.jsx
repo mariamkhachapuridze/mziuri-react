5d152743a6d5e6094c99f5ac591a5d15024c7012
mariamkhachapuridze <khachapuridzemariami1@gmail.com>
2025-10-29 22:56:47 +0400

    done

--- dice-game/src/App.jsx
+++ dice-game/src/App.jsx
@@ -1,6 +1,10 @@
 import {useState} from "react";
 import Dice from "./Dice.jsx";
 function App() {
+  const [player1Total,setPlayer1Total] = useState(0);
+  const [player2Total,setPlayer2Total] = useState(0);
+  const [round,setRound]= useState(0);
+  const [winner,setWinner]= useState("");
 
   const [player1, setPlayer1] = useState(0);
   const [player2, setPlayer2] = useState(0);
@@ -8,14 +12,36 @@ function App() {
 
   const rollDice = () => {
     const playerRandomNumber = Math.floor(Math.random() * 6);
+    setWinner("");
 
     if(currentPlayer === 1){
       setPlayer1(playerRandomNumber);
+      setPlayer1Total(player1Total + playerRandomNumber+1);
       setCurrentPlayer(2);
     } else {
+      let newTotal = player2Total + playerRandomNumber+1;
+
       setPlayer2(playerRandomNumber);
+      setPlayer2Total(newTotal);
       setCurrentPlayer(1);
+      setRound(round + 1);
+      if(round === 1){
+        if(player1Total > newTotal){
+          setWinner("Player 1 wins!");
+        } else if (newTotal > player1Total){
+          setWinner("Player 2 wins!");
+        } else {
+          setWinner("It's a tie!");
+        }
+        setRound(0);
+        setPlayer1Total(0);
+        setPlayer2Total(0);
+        setPlayer1(0);
+        setPlayer2(0);
+      }
     }
+
+
   }
   
   return <div
@@ -30,6 +56,10 @@ function App() {
   <div>VS</div>
   
     <Dice title={"Player 2"} index={player2} handleClick={rollDice} isDisabled={currentPlayer === 1}/>
+      <p>Player 1 total: {player1Total}</p>
+      <p>Player 2 total: {player2Total}</p>
+      <p>Round: {round }</p>
+      <p>{winner}</p>
   </div>
 }
 
